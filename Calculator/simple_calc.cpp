bfa73a24eae74fc95d7707b6c6e206452c7e0908
Tomer <72018284+parrot264@users.noreply.github.com>
2025-05-19 20:37:55 +0300

    Add files via upload

---
+++ Calculator/simple_calc.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int num;
+    int num2;
+    char op;
+    int sum;
+
+    cout << "Hi Welcome to my simple calc: \n";
+    cout << "First choose your first num: ";
+    cin >> num;
+    cout << "Now choose your second number: ";
+    cin >> num2;
+    cout << "Choose the op you want to do +,-,/,%,* \n";
+    cin >> op;
+
+    
+    switch (op)
+    {
+        case '+':
+            sum = num + num2;
+            cout << "resault: " << sum;
+            break;
+        case '-':
+            sum = num + num2;
+            cout << "resault: " << sum;
+            break;
+        case '/':
+            sum = num / num2;
+            cout << "resault: " << sum;
+            break;
+        case '%':
+            if (num2 != 0) {
+                sum = num % num2;
+                cout << "resault: " << sum;
+            }
+            else {
+                cout << "You can't modulo by zero";
+            }
+            break;
+        case '*':
+            sum = num * num2;
+            cout << "resault: " << sum;
+            break;
+        default:
+            cout << "unrecognized operation";
+            break;
+    }
+
+
+
+
+    
+
+}
+
+
